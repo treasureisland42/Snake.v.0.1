c65f558c132694b57282d4f7036906f16c4bd3b4
treasureisland42 <65664663+treasureisland42@users.noreply.github.com>
2020-05-27 13:48:40 +0100

    Updated Apple to create with main method, create, getters and setters
    Signed-off-by: treasureisland42 <65664663+treasureisland42@users.noreply.github.com>

--- src/main/java/Apple.java
+++ src/main/java/Apple.java
@@ -1,2 +1,39 @@
+import java.awt.*;
+import java.util.ArrayList;
+
 public class Apple {
+
+    private static Point points;
+    private static Apple a;
+    public Apple(Point p) {
+        points = p;
+    }
+
+    public static void main(String[] args){
+        a = spawnApple();
+    }
+    public static Apple spawnApple() {
+        int randX = (int) (Math.random() * 11);
+        int randY = (int) (Math.random() * 11);
+        Point p = new Point(randX, randY);
+        a = new Apple(p);
+        if(Snake.getHead().getX() == a.getP().getX() && Snake.getHead().getY() == a.getP().getY()){
+            spawnApple();
+        }
+        return a;
+    }
+
+    public static Point getP() {
+
+        return new Point((int) (points.getX()), (int) (points.getY()));
+    }
+
+    public static double getX() {
+        return a.getP().getX();
+    }
+
+    public static double getY() {
+        return a.getP().getY();
+    }
+
 }
